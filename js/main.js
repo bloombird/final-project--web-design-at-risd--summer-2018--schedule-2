12b2e3ba3b8426c66cb762dcac1865f453221ff4
bloombird <lishuofei92@gmail.com>
2018-07-19 23:39:27 -0400

    add girl svg animation

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@
         $leafInsideMask04 = $('#leaf4insideMask'),
         $leafInsideMask05 = $('#leaf5insideMask'),
         $leafInsideMask06 = $('#leaf6insideMask'),
+        $stage = $('#stage'),
+        $MainMask = $('#MainMask'),
+        $girl = $('#girl'),
+        $mouthStroke = $('#mouthStroke'),
+
         mainTl = new TimelineMax();
 
 
@@ -15,7 +20,12 @@
         var clearTl = new TimelineMax();
 
         clearTl
-            .set($plant, {x:0, y:80,cscale: 0.5, transformOrigin: 'center center'})
+            .set($plant, {x:50, y:80,cscale: 0.5, transformOrigin: 'center center'})
+            .set($girl, {autoAlpha: 1, x: '1400%', scale: 1.5, transformOrigin: 'bottom center'})
+            .set($stage, {autoAlpha:0.5})
+            .set($MainMask, {attr: {x: 1458}})
+
+            //leaf
             .set($leafInsideMask01, {attr: {y: 730}})
             .set($leafInsideMask02, {attr: {y: 696}})
             .set($leafInsideMask03, {attr: {y: 605}})
@@ -29,8 +39,21 @@
         return clearTl;
     }
 
+    function getIntroTl(){
+        var introTL = new TimelineMax();
+
+        introTL
+            .to($girl, 0.8, {x: '40%', ease: Power4.easeInOut});
+
+
+
+        return introTL;
+    }
+
     function init(){
-        mainTl.add(clearStage());
+        mainTl
+            .add(clearStage())
+            .add(getIntroTl(), 'scene-intro');
     }
     init();
 
